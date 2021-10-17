60081a4330bb90fa9d43e8b992a00881810d7349
Ubugeeei <ubuge1122@gamil.com>
2021-10-17 23:50:47 +0900

    implement bubble sort

--- src/algorithm/sort.rs
+++ src/algorithm/sort.rs
@@ -1,1 +1,2 @@
 pub mod bogo;
+pub mod bubble;
---
+++ src/algorithm/sort/bubble.rs
@@ -0,0 +1,28 @@
+#[allow(dead_code)]
+fn bubble_sort(numbers: &Vec<isize>) -> Vec<isize> {
+    let mut copy = numbers.clone();
+    let len = numbers.len();
+
+    for i in 0..len {
+        // iを引くことでlimitが上がっていく
+        for j in 0..len - 1 - i {
+            if copy[j] > copy[j + 1] {
+                let _j = copy[j + 1];
+                let _j_next = copy[j];
+                copy[j] = _j;
+                copy[j + 1] = _j_next;
+            }
+        }
+    }
+
+    copy
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_bubble_sort() {
+        assert_eq!(vec![1, 2, 3, 4, 6, 7], bubble_sort(&vec![3, 6, 2, 4, 1, 7]))
+    }
+}
