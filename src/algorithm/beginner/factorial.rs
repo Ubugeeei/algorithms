b5b58bd82111cda1007c1670f7e97c8bad31df38
Ubugeeei <ubuge1122@gamil.com>
2022-05-31 07:20:29 +0900

    wip Y comb

--- src/algorithm/beginner/factorial.rs
+++ src/algorithm/beginner/factorial.rs
@@ -9,6 +9,24 @@ fn factorial(n: u32) -> u32 {
     n * factorial(n - 1)
 }
 
+// type Y1<T> = fn(x:T) -> T;
+// type Y2<T> = fn(f: Y1<T>, x:T) -> T;
+// fn YFactory<T>(f:Y2<T>) -> Y1<T> {
+// let mut closure1: Y1<T>;
+
+// let closure2: fn(ff: Y2<T> , xx: T)-> T = |ff: Y2<T> , xx: T| {
+// ff(closure1, xx)
+// };
+
+// *closure1 = |x| {
+// let result = closure2(f, x);
+// closure1 = |_| { () }; // Fix memory leak
+// result
+// };
+
+// closure1
+// }
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -21,4 +39,13 @@ mod test {
         assert_eq!(1, factorial(1));
         assert_eq!(479001600, factorial(12));
     }
+
+    // fn factorial_Y_test() {
+    // let fac : Y2<i32> = |f:Y1<i32>, x:i32| {
+    // if x==0 { 1 } else { f(x-1) * x }
+    // };
+    // assert_eq!(120, YFactory(fac)(5));
+    // assert_eq!(1, YFactory(fac)(0));
+    // assert_eq!(479001600, YFactory(fac)(12));
+    // }
 }
