99375d0d76dc5a1aa784f768cce7e4ddf1b9d0c4
Ubugeeei <ubuge1122@gamil.com>
2022-08-06 19:25:28 +0900

    tweak: lcg

--- src/ramdom_number/lcg.rs
+++ src/ramdom_number/lcg.rs
@@ -1,28 +1,32 @@
 //! linear congruential generator
-//! 
+//! linear congruence method is a alglithm of generating pseudorandom number
+//! Xi = (A * Xi-1 + C) mod M
 #![allow(dead_code)]
 
-const MAX: u32 = 0xffff_ffff;
+const M: u32 = 0xffff_ffff;
+const A: u64 = 69069;
+/// if C = 0, then the algorithm is called multiplicative congruence
+const C: u64 = 1;
 
 struct Rand {
   seed: u32,
 }
 
 impl Rand {
-  fn new(x: u32) -> Rand {
-    Rand { seed: x }
+  fn new(seed: u32) -> Rand {
+    Rand { seed }
   }
 
-  /// return ∈ Int(0, MAX)
+  /// return ∈ Int(0, M)
   fn rand(&mut self) -> u32 {
     let x = self.seed as u64;
-    self.seed = ((69069 * x + 1) & MAX as u64) as u32;
+    self.seed = ((A * x + C) & M as u64) as u32;
     self.seed
   }
 
   /// return ∈ R(0, 1)
   fn rand2(&mut self) -> f64 {
-    (1.0 / (MAX as f64 + 1.0)) * self.rand() as f64
+    (1.0 / (M as f64 + 1.0)) * self.rand() as f64
   }
 }
 
@@ -34,10 +38,10 @@ mod tests {
   fn test_rand() {
     let mut r = Rand::new(0);
     for _ in 0..10 {
-      assert!(r.rand() < MAX);
+      assert!(r.rand() < M);
     }
 
-    let mut r = Rand::new(MAX);
+    let mut r = Rand::new(M);
     for _ in 0..10 {
       assert!(r.rand2() < 1.0 && r.rand2() >= 0.0);
     }
