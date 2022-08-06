656051c5abd3bfbc7719c099594c598a434dd1ad
Ubugeeei <ubuge1122@gamil.com>
2022-08-06 16:52:10 +0900

    remove: dead code

--- src/algorithm/basic/divisor.rs
+++ src/algorithm/basic/divisor.rs
@@ -18,40 +18,12 @@ mod test {
     #[test]
     fn tes_get_divisor() {
         assert_eq!(get_divisor(1), vec![1]);
-    }
-
-    #[test]
-    fn tes_get_divisor2() {
         assert_eq!(get_divisor(2), vec![1, 2]);
-    }
-
-    #[test]
-    fn tes_get_divisor3() {
         assert_eq!(get_divisor(3), vec![1, 3]);
-    }
-
-    #[test]
-    fn tes_get_divisor4() {
         assert_eq!(get_divisor(4), vec![1, 2, 4]);
-    }
-
-    #[test]
-    fn tes_get_divisor5() {
         assert_eq!(get_divisor(6), vec![1, 2, 3, 6]);
-    }
-
-    #[test]
-    fn tes_get_divisor6() {
         assert_eq!(get_divisor(12), vec![1, 2, 3, 4, 6, 12]);
-    }
-
-    #[test]
-    fn tes_get_divisor7() {
         assert_eq!(get_divisor(15), vec![1, 3, 5, 15]);
-    }
-
-    #[test]
-    fn tes_get_divisor8() {
         assert_eq!(get_divisor(24), vec![1, 2, 3, 4, 6, 8, 12, 24]);
     }
 }
