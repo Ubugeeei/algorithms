65a666b3486019d475631ab32963f5c5ade5d5b5
Ubugeeei <ubuge1122@gamil.com>
2022-08-06 17:42:24 +0900

    refactor: dir arc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod basic;
 pub mod divide_and_conquer;
 pub mod dp;
-pub mod sort;
+pub mod list;
 pub mod search;
+pub mod sort;
--- src/main.rs
+++
@@ -1,6 +0,0 @@
-mod algorithm;
-mod data_struct;
-
-fn main() {
-    println!("Hello, algorithms!");
-}
