4f354accff367e7ae25e21139ae7e2dc00b6cc89
Ubugeeei <ubuge1122@gamil.com>
2022-08-14 23:23:44 +0900

    test: sha256

--- src/crypto/hash/sha256.rs
+++ src/crypto/hash/sha256.rs
@@ -289,11 +289,18 @@ mod tests {
   #[test]
   fn test_sha256_exec() {
     let hasher = SHA256::new();
-    let message = String::from("hello world");
-    let res = hasher.exec(message);
+
     assert_eq!(
-      res,
+      hasher.exec(String::from("hello")),
+      "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+    );
+    assert_eq!(
+      hasher.exec(String::from("hello world")),
       "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
+    );
+    assert_eq!(
+      hasher.exec(String::from("あいうえお")),
+      "fdb481ea956fdb654afcc327cff9b626966b2abdabc3f3e6dbcb1667a888ed9a"
     );
   }
 }
