b7d4457009db54fd88a8c071c332fbb137db3a84
Ubugeeei <ubuge1122@gamil.com>
2022-08-14 23:49:33 +0900

    add sha256 spec comment

--- src/crypto/hash/sha256.rs
+++ src/crypto/hash/sha256.rs
@@ -1,4 +1,6 @@
 //! The SHA256 hash algorithm.
+//! spec: https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf
+//! 6.2 https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=23
 
 #![allow(dead_code)]
 
@@ -12,7 +14,9 @@ impl SHA256 {
    */
   const BLOCK_SIZE: usize = 64;
   const DELIMITER: u32 = 0x80;
+
   /// 4bytes after the decimal point of the cube root of 64 prime numbers from smallest to largest
+  /// https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=15
   const K: [u32; 64] = [
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -23,7 +27,9 @@ impl SHA256 {
     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
   ];
+
   /// 4bytes after the decimal point of the square root of 8 prime numbers from smallest to largest
+  /// 5.3.2  https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=18
   const H: [u32; 8] = [
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
   ];
@@ -60,6 +66,7 @@ impl SHA256 {
   }
 
   /// calculate the hash of the message
+  /// https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=24
   pub fn hash(self, blocks: Vec<[u32; 16]>) -> [u32; 8] {
     let mut state = SHA256::H.clone();
 
@@ -144,7 +151,7 @@ impl SHA256 {
 
   /// pre-prpcess
   /// add padding and sizes to the message
-  /// returns: 64 bytes as a u8 array
+  /// 5. https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=17
   pub fn add_padding(self, message: Vec<u8>) -> Vec<u8> {
     const SIZE_BYTES: usize = 8;
 
@@ -184,27 +191,35 @@ impl SHA256 {
   /**
    *
    * bit opes funuctions
+   * 
+   * https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=14
    *
    */
 
+  /// 4.2 https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=14
   fn ch(self, x: u32, y: u32, z: u32) -> u32 {
     (x & y) ^ (!x & z)
   }
+  /// 4.3 https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=14
   fn maj(self, x: u32, y: u32, z: u32) -> u32 {
     (x & y) ^ (x & z) ^ (y & z)
   }
 
+  /// 4.4 https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=14
   #[allow(non_snake_case)]
   fn SIGMA0(self, x: u32) -> u32 {
     x.rotate_right(2) ^ x.rotate_right(13) ^ x.rotate_right(22)
   }
+  /// 4.5 https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=14
   #[allow(non_snake_case)]
   fn SIGMA1(self, x: u32) -> u32 {
     x.rotate_right(6) ^ x.rotate_right(11) ^ x.rotate_right(25)
   }
+  /// 4.6 https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=14
   fn sigma0(self, x: u32) -> u32 {
     x.rotate_right(7) ^ x.rotate_right(18) ^ (x >> 3)
   }
+  /// 4.7 https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf#page=14
   fn sigma1(self, x: u32) -> u32 {
     x.rotate_right(17) ^ x.rotate_right(19) ^ (x >> 10)
   }
